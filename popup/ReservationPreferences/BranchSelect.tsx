07926a89d77896d86eaa75ffe3bb10b797eeef97
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2024-03-08 01:19:59 +0300

    add options for football and "TEVFİK AYDENİZ SPOR TESİSİ"

--- popup/ReservationPreferences/BranchSelect.tsx
+++ popup/ReservationPreferences/BranchSelect.tsx
@@ -1,9 +1,7 @@
 export const BranchSelect = ({ disabled = false, value }: ReservationPreferenceSelectProps) => (
 	<select disabled={disabled} value={value}>
 		<option value='59b7bd71-1aab-4751-8248-7af4a7790f8c'>Tenis</option>
-		<option hidden value='5bdbe5f0-a06e-4243-b65d-231b3c2247ed'>
-			Futbol
-		</option>
+		<option value='5bdbe5f0-a06e-4243-b65d-231b3c2247ed'>Futbol</option>
 		<option hidden value='b735aa4b-5923-4789-a04e-bf3e0b0a467c'>
 			Basketbol / Voleybol
 		</option>
